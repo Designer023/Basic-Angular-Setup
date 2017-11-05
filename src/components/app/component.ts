293c32518d14b25083611cfdd59ad827ef083de1
Carl Topham <carl@carl-topham.com>
2017-11-05 19:16:44 +0000

    fixing non-working scripts

--- src/components/app/component.ts
+++ src/components/app/component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './template.html' // The HTML template that describes the component's DOM structure including child components
+  templateUrl: './template.html'
 })
 
 export class AppComponent {}
